77924c00e7530e75a949e99f271fd3d25e4ab009
sarwadeshivaji3 <109013908+sarwadeshivaji3@users.noreply.github.com>
2022-10-09 22:07:10 +0530

    tse

--- FinanceManagementSystemDomainProject-1/src/main/java/com/example/demo/FinanceManagementSystemDomainProject1Application.java
+++ FinanceManagementSystemDomainProject-1/src/main/java/com/example/demo/FinanceManagementSystemDomainProject1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinanceManagementSystemDomainProject1Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(FinanceManagementSystemDomainProject1Application.class, args);
+	
 	}
 
 }
