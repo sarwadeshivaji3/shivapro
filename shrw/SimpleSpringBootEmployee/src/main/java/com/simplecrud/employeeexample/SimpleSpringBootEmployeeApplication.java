accafc1ae2e4730fcf247a19f5d21354127ccaf0
sarwadeshivaji3 <109013908+sarwadeshivaji3@users.noreply.github.com>
2022-10-12 08:15:49 +0530

    emp

--- shrw/SimpleSpringBootEmployee/src/main/java/com/simplecrud/employeeexample/SimpleSpringBootEmployeeApplication.java
+++ shrw/SimpleSpringBootEmployee/src/main/java/com/simplecrud/employeeexample/SimpleSpringBootEmployeeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimpleSpringBootEmployeeApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SimpleSpringBootEmployeeApplication.class, args);
 	}
 
